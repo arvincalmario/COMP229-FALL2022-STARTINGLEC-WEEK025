68e36e7a0b4c5ec7894af358dfb392ec37423d45
Arvin <arvin.almario07@gmail.com>
2022-09-17 19:49:24 -0400

    week2
    week 2 class

---
+++ Modules/main.js
@@ -0,0 +1,16 @@
+//imports the functions halfof from the lib.js module
+
+// import { halfOf, multiply } from './lib.js';
+// console.log("halfOf: " + halfOf(84) );
+// console.log("multiply:" + multiply(84, 2) );
+
+//do something example in lib.js
+// import doSomething from './lib.js';
+// doSomething();
+
+import { flag, touch } from './validator.js'; 
+console.log(flag); //false 
+touch(); // change to true 
+console.log(flag); // true 
+flag = false; // change to false 
+console.log(flag); // error
