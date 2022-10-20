f5143b2698fafd15fc414d4e595e26ccd8b6cf97
Arvin <arvin.almario07@gmail.com>
2022-10-20 02:29:27 -0400

    homework 2 initial commit

---
+++ routes/inventory.js
@@ -0,0 +1,20 @@
+// Student ID: 301225269
+// Name: Arvin Almario
+// Data: Oct 03, 2022
+
+var express = require('express');
+var router = express.Router();
+
+/* home page response. */
+router.get('/', function(req, res, next) {
+  res.render(
+    'inventory', 
+    { 
+      title: 'Home', 
+      mission: 'to provide out of the box ideas that gives my clients advantage to their business competitors.',
+      vision: 'to create a better everyday life for many people.', 
+    }
+  );
+});
+
+module.exports = router;
